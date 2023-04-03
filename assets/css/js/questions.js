2e540f94909426fc0f101d1644439778bf51e337
Cole Arrington <119268105+colearrington98@users.noreply.github.com>
2023-04-03 17:24:13 -0500

    Questions javascript for quiz

---
+++ assets/css/js/questions.js
@@ -0,0 +1,47 @@
+// list of questions, choices, and answers
+
+var questions = [
+    {
+        title: "What is a variable?",
+        choices: ["A variable is a container for a value", "A variable is a value", "A variable is a container for a container", "A variable is a container for a container for a value"],
+        answer: "A variable is a container for a value"
+    },
+    {
+        title: "What is a string?",
+        choices: ["A string is a container for a value", "A string is a value", "A string is a container for a container", "A string is a container for a container for a value"],
+        answer: "A string is a container for a container for a value"
+    },
+    {
+        title: "What is an array?",
+        choices: ["An array is a container for a value", "An array is a value", "An array is a container for a container", "An array is a container for a container for a value"],
+        answer: "An array is a container for a container for a value"
+    },
+    {
+        title: "What is a function?",
+        choices: ["A function is a container for a value", "A function is a value", "A function is a container for a container", "A function is a container for a container for a value"],
+        answer: "A function is a container for a container for a value"
+    },
+    {
+        title: "What is a boolean?",
+        choices: ["A boolean is a container for a value", "A boolean is a value", "A boolean is a container for a container", "A boolean is a container for a container for a value"],
+        answer: "A boolean is a value"
+    },
+    {
+        title: "What is an object?",
+        choices: ["An object is a container for a value", "An object is a value", "An object is a container for a container", "An object is a container for a container for a value"],
+        answer: "An object is a container for a container for a value"
+    },
+    {
+        title: "What is a prompt?",
+        choices: ["A prompt is a container for a value", "A prompt is a value", "A prompt is a container for a container", "A prompt is a container for a container for a value"],
+        answer: "A prompt is a container for a container for a value"
+    },
+    {
+        title: "What is a if statement?",
+        choices: ["An if statement is a container for a value", "An if statement is a value", "An if statement is a container for a container", "An if statement is a container for a container for a value"],
+        answer: "An if statement is a container for a container for a value"
+    },
+    
+];
+
+    
